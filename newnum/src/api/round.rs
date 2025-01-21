3bf097e154666f9927ebb9d5526f896c8c3f898d
Noam2Stein <noamsteindev@gmail.com>
2025-01-21 21:23:20 +0200

    IRound doc

--- newnum/src/api/round.rs
+++ newnum/src/api/round.rs
@@ -1,6 +1,7 @@
 use crate::WholeEquivalent;
 
-/// Trait for the round API (`round`, `floor`...) which may be used for numbers, number containers, or anything that makes sense.
+/// Trait for the round API (`round`, `floor`...)
+/// which may be used for numbers, number containers, or anything that makes sense.
 ///
 /// Functions in this trait return `Self` so this isn't round-to-int,
 /// for that use [`IRound`].
@@ -24,31 +25,53 @@ pub trait Round: Sized {
     /// For example: `1.2 => 2.0`, `1.8 => 2.0`, `-1.3 => -1.0`.
     fn ceil(self) -> Self;
 
-    /// If positive rounds down, else rounds up.
+    /// Rounds towards zero. If positive rounds down, else rounds up.
     ///
-    /// Effectively removes the fractional part of the number by rounding towards zero.
+    /// Effectively removes the fractional.
     /// For example: `1.2 => 1.0`, `1.8 => 1.0`, `-1.3 => -1.0`.
     fn trunc(self) -> Self;
 
-    /// If positive rounds up, else rounds down.
+    /// Rounds away from zero. If positive rounds up, else rounds down.
     ///
-    /// Effectively rounds away from zero.
     /// For example: `1.2 => 2.0`, `1.8 => 2.0`, `-1.3 => -2.0`.
     fn atrunc(self) -> Self;
 }
 
-/// Trait for a round-to-int API (iround, ifloor...).
+/// Trait for the round-to-int API (`round_to_int = iround`, `floor_to_int = ifloor`...)
+/// which may be used for numbers, number containers, or anything that makes sense.
 ///
-/// ```Round``` returns ```Self```,
-/// where ```IRound``` returns ```Self::Whole``` which represents the whole equivalent of ```Self```.
+/// Functions in this trait return `Self::Whole` which is the associated int type that can represent `Self`'s range.
+/// for normal round (not-to-int) use [`Round`].
 ///
-/// Doesn't require ```Self``` to be a number because non-number types can benefit from this too.
-/// For example number containers like vectors or matricies.
+/// Can also and should also be implemented for types that only represent round numbers (int types).
+/// This is because if you have a `value: impl Num`,
+/// it makes sense to round it even if it may be an int meaning already round.
+/// Keep in mind that `Self: Whole` means `Self: WholeEquivalent<Whole = Self>`.
 pub trait IRound: Sized + WholeEquivalent {
+    /// Rounds to the closer whole number, and converts into an integer.
+    ///
+    /// For example: `1.2 => 1`, `1.8 => 2`, `-1.3 => -1`.
     fn iround(self) -> Self::Whole;
+
+    /// Rounds down, and converts into an integer.
+    ///
+    /// For example: `1.2 => 1`, `1.8 => 1`, `-1.3 => -2`.
     fn ifloor(self) -> Self::Whole;
+
+    /// Rounds up, and converts into an integer.
+    ///
+    /// For example: `1.2 => 2`, `1.8 => 2`, `-1.3 => -1`.
     fn iceil(self) -> Self::Whole;
+
+    /// Rounds towards zero (if positive rounds down, else rounds up), and converts into an integer.
+    ///
+    /// Effectively removes the fractional.
+    /// For example: `1.2 => 1`, `1.8 => 1`, `-1.3 => -1`.
     fn itrunc(self) -> Self::Whole;
+
+    /// Rounds away from zero (if positive rounds up, else rounds down), and converts into an integer.
+    ///
+    /// For example: `1.2 => 2`, `1.8 => 2`, `-1.3 => -2`.
     fn iatrunc(self) -> Self::Whole;
 }
 
