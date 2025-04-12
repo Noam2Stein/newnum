b0a0b0af9f0e000d14a083c5634e0c0935170725
Noam2Stein <noamsteindev@gmail.com>
2025-04-12 11:23:52 +0300

    root doc update

--- newnum/src/api/root.rs
+++ newnum/src/api/root.rs
@@ -1,12 +1,16 @@
 /// Trait for `trunc_sqrt` and `trunc_cbrt` methods, which compute the truncated square root and cube root of a number.
 ///
-/// For non-numbers type (number-containers like `Vec2`) the logic of `sqrt` / `cbrt` should follow the logic of `Mul`.
-/// For example, if `<Vec2 as Mul>` multiplies each component seperately, it should also `sqrt` each component seperately.
+/// For non-numbers types (number-containers like `Vec2`) the logic of `sqrt` / `cbrt` should follow the logic of `Mul`.
+/// For example, if `<Vec2 as Mul>` multiplies each component seperately, <Vec2 as TruncRoot> should also `sqrt` each component seperately.
 pub trait TruncRoot {
     fn trunc_sqrt(self) -> Self;
     fn trunc_cbrt(self) -> Self;
 }
 
+/// Trait for `sqrt` and `cbrt` methods, which compute the square root and cube root of a number.
+///
+/// For non-numbers types (number-containers like `Vec2`) the logic of `sqrt` / `cbrt` should follow the logic of `Mul`.
+/// For example, if `<Vec2 as Mul>` multiplies each component seperately, <Vec2 as Root> should also `sqrt` each component seperately.
 pub trait Root: TruncRoot {
     fn sqrt(self) -> Self;
     fn cbrt(self) -> Self;
